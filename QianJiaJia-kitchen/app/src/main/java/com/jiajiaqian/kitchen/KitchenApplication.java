69cb076edd413c1b562d1b447b294499897628f3
t_xuz <zhebao.xu@autodesk.com>
2016-12-13 23:27:51 +0800

    [kitchen]首页添加底部tab列表

---
+++ QianJiaJia-kitchen/app/src/main/java/com/jiajiaqian/kitchen/KitchenApplication.java
@@ -0,0 +1,16 @@
+package com.jiajiaqian.kitchen;
+
+import android.app.Application;
+
+/**
+ * Created by t_xuz on 12/13/16.
+ *
+ */
+
+public class KitchenApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- QianJiaJia-kitchen/app/src/main/java/com/jiajiaqian/kitchen/ui/MainActivity.java
+++ QianJiaJia-kitchen/app/src/main/java/com/jiajiaqian/kitchen/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jiajiaqian.kitchen;
+package com.jiajiaqian.kitchen.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jiajiaqian.kitchen.R;
 
 public class MainActivity extends AppCompatActivity {
 
