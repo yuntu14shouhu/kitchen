bb270697c187baf369f781af1f85621cc8b1ef31
jiajia <jj1234tl@gmail.com>
2017-04-07 17:29:33 +0800

    【kitchen】bean修改

--- QianJiaJia-kitchen/app/src/main/java/com/jiajiaqian/kitchen/common/entity/UserBean.java
+++ QianJiaJia-kitchen/app/src/main/java/com/jiajiaqian/kitchen/common/entity/UserBean.java
@@ -22,41 +22,37 @@ public class UserBean implements Serializable{
      * userName : string
      */
 
-    private int age;
-    private String contacts;
-    private String createTime;
+    /**
+     * 2017-04-07--16:00:00修改为-----------------
+     *
+     * id : a240611ed1174b4dbf3d6536517d3b6c
+     * userName : string46
+     * password : 60df882a80f5c8bca08b3aeb0025fb561daa65bdcadb38dd4ea2376a3fd43f5934aa956453598cb23b83965adee87a7d4163d6d8989b52c57cef07befc021116
+     * nickName : string
+     * salt : kitchen
+     * status : 0
+     * sex : string
+     * age : 0
+     * createTime : 1491471599552
+     * remark : string
+     * email : string
+     * phone : string111
+     * qqnumber : string
+     */
+
     private String id;
-    private String nickName;
-    private String password;
-    private String remark;
-    private String salt;
-    private String sex;
-    private int status;
     private String userName;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getContacts() {
-        return contacts;
-    }
-
-    public void setContacts(String contacts) {
-        this.contacts = contacts;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
+    private String password;
+    private String nickName;
+    private String salt;
+    private int status;
+    private String sex;
+    private int age;
+    private long createTime;
+    private String remark;
+    private String email;
+    private String phone;
+    private String qqnumber;
 
     public String getId() {
         return id;
@@ -66,12 +62,12 @@ public class UserBean implements Serializable{
         this.id = id;
     }
 
-    public String getNickName() {
-        return nickName;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getPassword() {
@@ -82,12 +78,12 @@ public class UserBean implements Serializable{
         this.password = password;
     }
 
-    public String getRemark() {
-        return remark;
+    public String getNickName() {
+        return nickName;
     }
 
-    public void setRemark(String remark) {
-        this.remark = remark;
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
     }
 
     public String getSalt() {
@@ -98,14 +94,6 @@ public class UserBean implements Serializable{
         this.salt = salt;
     }
 
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
     public int getStatus() {
         return status;
     }
@@ -114,11 +102,59 @@ public class UserBean implements Serializable{
         this.status = status;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getSex() {
+        return sex;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getQqnumber() {
+        return qqnumber;
+    }
+
+    public void setQqnumber(String qqnumber) {
+        this.qqnumber = qqnumber;
     }
 }
